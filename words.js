d9d240933d7dab3e885f238698000750c38343c0
angginursrgh <150356133+angginursrgh@users.noreply.github.com>
2023-11-11 09:28:41 +0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,62 @@
+const words = [
+    {
+        word: "MERIAM",
+        hint: "Jika PULANG = VARGTM, maka 'GYLCUG' ="
+    },
+    {
+        word: "LAYANAN",
+        hint: "Jika MAIN = KCKL, maka 'NYAYPYP' = "
+    },
+    {
+        word: "PELAJARAN",
+        hint: "Jika KAOS = HXLP, maka 'SHODMDUDQ' = "
+    },
+    {
+        word: "ZXIX",
+        hint: "Jika PENA = NHLD, maka 'BUKU' = "
+    },
+    {
+        word: "HDIPHVI",
+        hint: "Jika MAKANAN = HVFVIVI, maka 'MINUMAN' = "
+    },
+    {
+        word: "PAGI JUGA",
+        hint: "Jika WHODPDW SDJL = SELAMAT PAGI, maka 'SDJL MXJD' = "
+    },
+    {
+        word: "IDZBERLH",
+        hint: "Jika WHATSAPP = ZKXQVDMM, maka 'FACEBOOK' = "
+    },
+    {
+        word: "AKUNTANSI",
+        hint: "Jika LZSDLZSHJZ = MATEMATIKA, maka 'ZJTMSZMRH' = "
+    },
+    {
+        word: "TELEVISI",
+        hint: "Jika MPOQWUGS = KOMPUTER, maka 'VFNFXJUJ' = "
+    },
+    {
+        word: "IJWNYF",
+        hint: "Jika BAHAGIA = GFMFLNF, maka 'DERITA' = "
+    },
+    {
+        word: "HMAUR",
+        hint: "Jika BILIK = KLIIB, maka RUANG = GANUR, maka 'RUMAH' = "
+    },
+    {
+        word: "TXAXE",
+        hint: "Jika BOTOL = YLQLI, maka 'WADAH' = "
+    },
+    {
+        word: "ARTUBM",
+        hint: "Jika BENANG = EBGNAN, maka 'RAMBUT' = "
+    },
+    {
+        word: "BELMR",
+        hint: "Jika KERTAS = TEKRS, maka 'LEMBAR' = "
+    },
+]
+
+
+
+
